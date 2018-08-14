e9443c2697487b254cab71173847f966ae03210d
warrick <wcor690@aucklanduni.ac.nz>
2018-08-14 15:09:54 +1200

    reset to roll back then updated to remove inbetween history

--- endangeredfinder.py
+++ endangeredfinder.py
@@ -1,4 +1,5 @@
 import csv
+import re
 import requests
 from collections import OrderedDict
 
@@ -9,46 +10,16 @@ def _get_search_data(term):
     results = response_json['data']
     return results 
 
-# with open('risk_organisms_Simon_Feb18.csv', 'r') as csvfile:
-#     with open('output.csv', 'w') as output_file:
-#         reader = csv.reader(csvfile)
-#         fieldnames = ["query", "term", "otu", "site", "value"]
-#         writer = csv.DictWriter(output_file, fieldnames)
-#         for line in reader:
-#             output_dict = OrderedDict({
-#                 'query': "",
-#                 'term': "",
-#                 'otu': "",
-#                 'site':"",
-#                 'value':""
-#             })
-#             output_dict['query'] = line[0]
-#             for term in line[0].split(' '):
-#                 if term == '':
-#                     continue
-#                 output_dict["term"] = term
-#                 for otu_entry in _get_search_data(term):
-#                     if len(_get_search_data) == 0:
-#                         break
-#                     output_dict["otu"] = otu_entry['']
-#                     del otu_entry['']
-#                     for site in otu_entry:
-#                         output_dict["site"] = site
-#                         output_dict["value"] = otu_entry[site]
-#             if (output_dict['otu'] != "" and output_dict['value'] != ""):
-#                 print(output_dict)
-#                 writer.writerow(output_dict)
-
-
 with open('risk_organisms_Simon_Feb18.csv', 'r') as csvfile:
     with open('output.csv', 'w') as output_file:
         reader = csv.reader(csvfile)
-        fieldnames = ["query", "term", "otu", "site", "value"]
         writer = csv.writer(output_file, delimiter=",")
         for line in reader:
             query = line[0]
+            cleaned_query = re.sub(r'(?:(?<=\().+?(?=\))|(?<=\[).+?(?=\]))', "", line[0])
             # print(query)  
-            for term in query.split(' '):
+            for term in cleaned_query.split(' '):
+                
                 if (term != ''):
                     # print(term)
                     otus = _get_search_data(term)
