16a2a34863ad6a2c7cbd4adee29b577794a23e35
warrick <wcor690@aucklanduni.ac.nz>
2019-05-16 13:49:18 +1200

    updated urls and response structure. Need to sort missing site ids.

--- endangeredfinder.py
+++ endangeredfinder.py
@@ -3,36 +3,75 @@ import re
 import requests
 from collections import OrderedDict
 
-def _get_search_data(term):
-    url = format('https://edna.nectar.auckland.ac.nz/edna/api/abundance?term=%s' % term)
+# 'dev': 'http://localhost:8000/edna/api/v1.0/abundance?otu=',
+# 'prod': 'https://edna.nectar.auckland.ac.nz/enda/api/v1.0/abundance?otu=',
+
+sample_otu_dev_url = 'http://localhost:8000/edna/api/v1.0/abundance?otu='
+
+otu_dev_url = 'http://localhost:8000/edna/api/v1.0/abundance?otu='
+
+site_dict = {}
+otu_dict = {}
+
+def _get_search_data(text):
+    # url = format('https://edna.nectar.auckland.ac.nz/edna/api/abundance?otu=&term=%s' % term)
+    term = "&text=" + text
+    url = format(sample_otu_dev_url + term)
+    print(url)
+
     response = requests.get(url)
-    response_json = response.json()
-    results = response_json['data']
+    results = response.json()
+    print(results)
     return results 
 
-with open('risk_organisms_Simon_Feb18.csv', 'r') as csvfile:
+def _create_site_lookup(sites):
+    '''iterates the site metadata array and organises them by id as key'''
+    if site_dict:
+        return
+    for site in sites:
+        print(site)
+        site_dict[site['id']] = site
+
+def get_otu_info(otu_id):
+    ''' queries for info regarding an otu. Tries local dict first then resorts to querying api as last resort'''
+    if otu_id in otu_dict:
+        return otu_dict[otu_id]
+    else:
+        url = 'http://localhost:8000/edna/api/v1.0/otu/' + str(otu_id)
+        response = requests.get(url)
+        json = response.json()
+        name = json['otu_names'][0]
+        print(name)
+
+
+with open('./endangered_files/risk_organisms_Simon_Feb18.csv', 'r') as csvfile:
     with open('output.csv', 'w') as output_file:
         reader = csv.reader(csvfile)
         writer = csv.writer(output_file, delimiter=",")
         for line in reader:
             query = line[0]
+            # re sub removed '(' ')' '[' ']' and replaces with ''
             cleaned_query = re.sub(r'(?:(?<=\().+?(?=\))|(?<=\[).+?(?=\]))', "", line[0])
-            # print(query)  
-            for term in cleaned_query.split(' '):
-                
-                if (term != ''):
+            print(cleaned_query)  
+            for endangered_segment in cleaned_query.split(' '):
+                if (endangered_segment != ''):
                     # print(term)
-                    otus = _get_search_data(term)
-                    if len(otus) > 0:
-                        # print(otus)
-                        for otu in otus:
-                            for field in otu:
-                                if field == "":
-                                    otu_name = otu[field]
-                                    print(otu_name)
-                                    continue
-                                site = field
-                                value = otu[field]
-                                writer.writerow([query, term, otu_name, site, value])
+                    response_data = _get_search_data(endangered_segment)
+                    sample_otus = response_data['sample_otu_data']
+                    _create_site_lookup(response_data['sample_contextual_data']) 
+                    if len(sample_otus) > 0:
+                        for sample_otu in sample_otus:
+                            print(sample_otu)
+                            otu_name = get_otu_info(sample_otu[0])
+                            sample_identifier = site_dict[sample_otu[1]]
+                            value = [sample_otu[2]]
+                            # for field in sample_otu:
+                            #     if field == "":
+                            #         otu_name = sample_otu[field]
+                            #         print(otu_name)
+                            #         continue
+                            #     site = field
+                            #     value = sample_otu[field]
+                            writer.writerow([query, endangered_segment, otu_name, sample_identifier, value])
                     else:
-                        writer.writerow([query, term])
+                        writer.writerow([query, endangered_segment])
