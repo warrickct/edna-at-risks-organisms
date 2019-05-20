d89b26486d4d94a9d1814548369252eeba7073e4
warrick <wcor690@aucklanduni.ac.nz>
2019-05-20 13:48:02 +1200

    iterates and outputs to separate files based on input file name. Queries database only once per organism based on more specific taxononmic classifcation between genus and species.

--- risk_organism_finder.py
+++ risk_organism_finder.py
@@ -1,6 +1,7 @@
 import csv
 import re
 import requests
+import glob
 from collections import OrderedDict
 
 # 'dev': 'http://localhost:8000/edna/api/v1.0/abundance?otu=',
@@ -13,7 +14,7 @@ otu_dev_url = 'http://localhost:8000/edna/api/v1.0/abundance?otu='
 site_dict = {}
 otu_dict = {}
 
-def _get_search_data(text):
+def _get_sample_otus(text):
     # url = format('https://edna.nectar.auckland.ac.nz/edna/api/abundance?otu=&term=%s' % term)
     term = "&text=" + text
     url = format(sample_otu_dev_url + term)
@@ -21,13 +22,6 @@ def _get_search_data(text):
     response = requests.get(url)
     results = response.json()
     return results 
-
-def _create_site_lookup(sites):
-    '''iterates the site metadata array and organises them by id as key'''
-    if site_dict:
-        return
-    for site in sites:
-        site_dict[site['id']] = site
 
 def get_site_info(site_id):
     ''' attempts to get sample identifier from lookup, if fails then requests from database.'''
@@ -54,35 +48,52 @@ def get_otu_info(otu_id):
         name = json['otu_names'][0]
         return name
 
+def search_and_write_row(organism, taxon):
+    response_data = _get_sample_otus(taxon)
+    sample_otus = response_data['sample_otu_data']
+    # _create_site_lookup(response_data['sample_contextual_data']) 
+    if len(sample_otus) > 0:
+        for sample_otu in sample_otus:
+            otu_name = get_otu_info(sample_otu[0])
+            # sample_identifier = site_dict[sample_otu[1]]
+            sample_identifier = get_site_info(sample_otu[1])
+            value = [sample_otu[2]]
+            writer.writerow([organism, taxon, otu_name, sample_identifier, value])
+    else:
+        writer.writerow([organism, taxon])
 
-with open('./endangered_files/risk_organisms_Simon_amended-combined-17-may-2019.csv', 'r') as csvfile:
-    with open('output.csv', 'w') as output_file:
-        reader = csv.reader(csvfile)
-        writer = csv.writer(output_file, delimiter=",")
-        header = ["endangered organisms", "organism subterm queried", "organism matched", "containing sample", "abundance"]
-        writer.writerow(header)
-        for line in reader:
-            query = line[0]
-            # re sub removed '(' ')' '[' ']' and replaces with ''
-            cleaned_query = re.sub(r'(?:(?<=\().+?(?=\))|(?<=\[).+?(?=\]))', "", line[0])
-            for endangered_segment in cleaned_query.split(' '):
-                if (endangered_segment != ''):
-                    response_data = _get_search_data(endangered_segment)
-                    sample_otus = response_data['sample_otu_data']
-                    # _create_site_lookup(response_data['sample_contextual_data']) 
-                    if len(sample_otus) > 0:
-                        for sample_otu in sample_otus:
-                            otu_name = get_otu_info(sample_otu[0])
-                            # sample_identifier = site_dict[sample_otu[1]]
-                            sample_identifier = get_site_info(sample_otu[1])
-                            value = [sample_otu[2]]
-                            # for field in sample_otu:
-                            #     if field == "":
-                            #         otu_name = sample_otu[field]
-                            #         print(otu_name)
-                            #         continue
-                            #     site = field
-                            #     value = sample_otu[field]
-                            writer.writerow([query, endangered_segment, otu_name, sample_identifier, value])
-                    else:
-                        writer.writerow([query, endangered_segment])
+paths = glob.glob('./endangered_files/*.csv')
+for path in paths:
+    print(path)
+    with open(path, 'r') as csvfile:
+        with open(path + '-output', 'w') as output_file:
+            reader = csv.reader(csvfile)
+            writer = csv.writer(output_file, delimiter=",")
+            header = ["endangered organisms", "organism subterm queried", "organism matched", "containing sample", "abundance"]
+            writer.writerow(header)
+            for row in reader:
+                for field in row:
+                    field = re.sub(r'(?:(?<=\().+?(?=\))|(?<=\[).+?(?=\]))', "", field)
+                genus = row[0]
+                species = row[1]
+                organism = genus + " " + species
+
+                if species:
+                    species = "s__" + species
+                    search_and_write_row(organism, species)
+                elif genus:
+                    genus = "g__" + genus
+                    search_and_write_row(organism, genus)
+                # if (endangered_segment != ''):
+                #     response_data = _get_search_data(endangered_segment)
+                #     sample_otus = response_data['sample_otu_data']
+                #     # _create_site_lookup(response_data['sample_contextual_data']) 
+                #     if len(sample_otus) > 0:
+                #         for sample_otu in sample_otus:
+                #             otu_name = get_otu_info(sample_otu[0])
+                #             # sample_identifier = site_dict[sample_otu[1]]
+                #             sample_identifier = get_site_info(sample_otu[1])
+                #             value = [sample_otu[2]]
+                #             writer.writerow([genus, endangered_segment, otu_name, sample_identifier, value])
+                #     else:
+                #         writer.writerow([genus, endangered_segment])
